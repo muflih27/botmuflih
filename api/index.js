645867ed3856a036162de411350a3ae208aa0bc2
muflih27 <52996668+muflih27@users.noreply.github.com>
2021-12-06 20:01:46 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '5046034896:AAEYsXC0p2t36pfQ0vgHX0FR0lV5PcPJ3IQ'
 const bot = new TelegramBot(token, {polling: true});
 
 
